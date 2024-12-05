31dead69a8051161212829956b126cf134cb4688
Rob Tuck <rctuck@princeton.edu>
2024-12-05 16:52:14 -0500

    Adding part 2 for Day 4

--- aoc4-2.py
+++ aoc4-2.py
@@ -1,104 +1,52 @@
 import numpy as np
 
 # # example data
-text = """
-MMMSXXMASM
-MSAMXMSMSA
-AMXSXMAAMM
-MSAMASMSMX
-XMASAMXAMM
-XXAMMXXAMA
-SMSMSASXSS
-SAXAMASAAA
-MAMMMXMMMM
-MXMXAXMASX
-"""
+# text = """
+# MMMSXXMASM
+# MSAMXMSMSA
+# AMXSXMAAMM
+# MSAMASMSMX
+# XMASAMXAMM
+# XXAMMXXAMA
+# SMSMSASXSS
+# SAXAMASAAA
+# MAMMMXMMMM
+# MXMXAXMASX
+# """
 
-reports = []
-# for line in data.strip().split('\n'):
-#     levels = list(line)
-#     reports.append(levels)
-
-
-#     for line in file:
-#         line.strip()
-#         levels = list(line)
-#         reports.append(levels)
-
-# np_reports = np.array(reports)
-
-# print(np_reports)
-
-# np_reports = np.fromfile('aoc4.data.txt', dtype='S1')
-# with open('aoc4.data.txt') as file:
-#     text = file.read()
-
+with open('aoc4.data.txt') as file:
+    text = file.read()
 
 test_str = []
 
 np_reports = np.array([list(line) for line in text.strip().split('\n')])
-test_str.append('\n'.join([''.join(row) for row in np_reports]))
 
-# for row in np_reports:
-    # print(''.join(row))
+test_mask = np.array([
+    ["M", "1", "S"], ["1", "A", "1"], ["M", "1", "S"]
+])
 
+generic_mask = np.array([
+    ["M", ".", "S"], [".", "A", "."], ["M", ".", "S"]
+])
 
-rotated90 = np.rot90(np_reports)
-test_str.append('\n'.join([''.join(row) for row in rotated90]))
+t2 = np.equal(test_mask, generic_mask)
 
-# for row in rotated90:
-#     print(''.join(row))
+temp_reports = np_reports.copy()
 
-rotated180 = np.rot90(rotated90)
-test_str.append('\n'.join([''.join(row) for row in rotated180]))
-# for row in rotated180:
-#     print(''.join(row))
+count = 0
+for i in range(4):
+    for r in range(1,np.shape(temp_reports)[0]-1):
+        for c in range(1,np.shape(temp_reports)[0]-1):
+            test = temp_reports[r-1:r+2, c-1:c+2]
+            check = np.equal(test, test_mask)
+            # print((check == t2).all())
+            # print(type(check))
+            # print(check, "\n", b_mask)
+            if (check == t2).all():
+                # print(f'Found at {r},{c}')
+                count += 1
+            # print(test)
+            # print('---')
+    temp_reports = np.rot90(temp_reports)
 
-rotated270 = np.rot90(rotated180)
-test_str.append('\n'.join([''.join(row) for row in rotated270]))
-
-# for row in rotated270:
-#     print(''.join(row))
-
-# print(rotated90)
-# print(rotated180)
-# print(rotated270)
-
-
-
-for i in range(-len(np_reports), len(np_reports)):
-    current_diagonal = np.diagonal(np_reports, i)
-    result = ''.join(current_diagonal)
-    test_str.append(result)
-    # print(result)
-
-for i in range(-len(np_reports), len(np_reports)):
-    current_diagonal = np.diagonal(rotated90, i)
-    result = ''.join(current_diagonal)
-    test_str.append(result)
-    # print(result)
-
-for i in range(-len(np_reports), len(np_reports)):
-    current_diagonal = np.diagonal(rotated180, i)
-    result = ''.join(current_diagonal)
-    test_str.append(result)
-    # print(result)
-
-for i in range(-len(np_reports), len(np_reports)):
-    current_diagonal = np.diagonal(rotated270, i)
-    result = ''.join(current_diagonal)
-    test_str.append(result)
-    # print(result)
-
-
-import re
-
-regex = r"XMAS"
-
-trial = ('\n'.join(test_str))
-
-print(trial)
-
-matches = re.findall(regex, trial, re.MULTILINE)
-
-print(len(matches))
+print(count)
