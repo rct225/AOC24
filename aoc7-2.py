c6a65900f3e52b928652fa5ec10d6b4d3828f94e
Rob Tuck <rctuck@princeton.edu>
2025-01-09 15:23:59 -0500

    Day 7 & 8 just a bit late!

---
+++ aoc7-2.py
@@ -0,0 +1,78 @@
+# # example
+
+from itertools import chain, zip_longest
+import math
+
+
+text = """
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+"""
+
+#     # for line in file:
+#     #     line.strip()
+#     #     levels = list(line)
+#     #     reports.append(levels)
+
+
+
+with open('aoc7.data.txt') as file:
+    text = file.read()
+
+reports = []
+for line in text.strip().split('\n'):
+    # levels = list(line)
+    reports.append(line)
+
+# np_reports = np.array(reports)
+
+# print(np_reports)
+
+addition = lambda a,b: a+b
+multiplication = lambda a,b: a*b
+concat = lambda a,b: int(str(a) + str(b))
+
+operators = [addition, multiplication, concat]
+
+
+def evaluate(values: list, testValue: int):
+    if(len(values) == 1): 
+        return values[0] == testValue
+    for op in operators:
+        value = op(values[0], values[1])
+        if(evaluate([value] + values[2::], testValue)):
+            return True
+    return False
+
+
+total = 0
+for rep in reports:
+    result, values = rep.split(':')
+    values = list(map(int, values.split()))
+    result = int(result)
+    # expr = []
+    # mods = [int(result) % v for v in values]
+    # ops = []
+    # for i in range(len(mods)-1):
+    #     if mods[i] == mods[i+1]:
+    #         ops += ["*"]
+    #         i += 1
+    #     else:
+
+    #         ops += ["+"]
+    # print(ops)
+    # print(values)
+    # print(result)
+
+    # print(evaluate(values,result))
+    if evaluate(values,result):
+        total += result
+
+print(total)
---
+++ aoc8-2.py
@@ -0,0 +1,88 @@
+import itertools
+import math
+from matplotlib import pyplot as plt
+import numpy as np
+
+# # example
+
+text = """
+............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............
+"""
+
+with open('aoc8.data.txt') as file:
+    text = file.read()
+
+reports = []
+for line in text.strip().split('\n'):
+    levels = list(line)
+    reports.append(levels)
+
+np_reports = np.array(reports)
+# print(np_reports.shape)
+
+# print(np_reports)
+
+antinodes = set()
+def new_point_on_line(p, q, distance):
+    """Finds a new point along the line defined by (x1, y1) and (x2, y2) at a given distance from (x1, y1)."""
+
+
+    # Calculate the length of the line segment
+    # length = math.dist(p, q)
+
+    x1, x2 = p[0], q[0]
+    y1, y2 = p[1], q[1]
+
+    antinodes.add((x2, y2))
+
+    # # Calculate the unit vector in the direction of the line
+    # unit_x = (x2 - x1) / length
+    # unit_y = (y2 - y1) / length
+
+    # Calculate the new point's coordinates
+    new_x = x2 + (x2 - x1)
+    new_y = y2 + (y2 - y1)
+
+    if new_x < np_reports.shape[0] and new_y < np_reports.shape[1] and new_x >= 0 and new_y >= 0:
+        new_point_on_line(q, (new_x, new_y), distance)
+    # antinodes.add((new_x, new_y))
+
+    # return int(new_x), int(new_y)
+
+freqs = set(np_reports[np_reports != '.'])
+
+pt = []
+for i, freq in enumerate(freqs):
+    points = np.array(np.where(np_reports == freq)).T
+    for combination in itertools.combinations(points, 2):
+        p = combination[0]
+        q = combination[1]
+        distance = math.dist(p, q)  
+        # print(p, q, distance)
+        p1 = new_point_on_line(p, q, distance * 2)
+        pt += [p1]
+        # print(p1)
+        p2 = new_point_on_line(q, p, distance * 2)
+        pt += [p2]
+        # print(p2)
+        # pt[(x, y)] = freq
+
+# print(pt, len(pt))
+
+print(len(antinodes))
+# filtered_pt = [(x,y) for x, y in pt if x < np_reports.shape[0] and y < np_reports.shape[1] and x >= 0 and y >= 0]
+# print(filtered_pt, len(filtered_pt))
+# print(set(filtered_pt), len(set(filtered_pt)))
+# print(len(set(filtered_pt)))
+# print(set(pt), len(set(pt)))
